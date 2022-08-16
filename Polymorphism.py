116867768b5c243b75e1536478e94b1e516bdb3e
Apurbasi011 <92560286+Apurbasi011@users.noreply.github.com>
2022-08-16 17:32:04 +0530

    Add files via upload

---
+++ Polymorphism.py
@@ -0,0 +1,36 @@
+class History_in_space():
+    def year_2008(self):
+        print("\n","Events Happened In Space 2008", "\n")
+        print("India Successfully Lauched Chandrayan in 2008 in Orde To Explore The Moon. The Craft Was Inserted Into Lunar Orbit In Its First Attempt.One Of THe GReates Achievement Of Chandrayan Was The Discovery Of Water In The Lunar Soil")
+        
+    def year_2011(self):
+        print("\n", "Event Occurred In The Field Of Space In 2011", "\n")
+        
+class History_in_medical():
+    def year_2008(self):
+        print("\n","Events Happened In Medical 2008", "\n")
+        print("Although Scientists Have Discovered How Malaria IS Transferred and Know How To Prevent It, Nearly 1 Million People Die Every Year Fro  The Disease, According To International Estimates. Finally, Dec.8, The First Results Of A Malaria Vaccine That Shows Promise Hit International News.")
+        
+    def year_2011(self):
+        print("\n","Events Occurred In The Field Of Medical in 2011", "\n")
+        
+class History_in_sports():
+    def year_2008(self):
+        print("\n","Events Happened In Sports 2008", "\n")
+        print("In 2008, Abhinav Bindra Created History By Bagging Gold In Men's 10m Air Rifle Event At The Beijing Games. His Gold Remains The Country's First AN dOnly Individual Gold MEdal Till Date.")
+        
+    def year_2011(self):
+        print("\n", "Events Occurred In The Field Of Sports In 2011",  "\n")
+        
+space = History_in_space()
+space.year_2008()
+space.year_2011()
+
+medical = History_in_medical()
+medical.year_2008()
+medical.year_2011()
+
+sports = History_in_sports()
+sports.year_2008()
+sports.year_2011()
+
